0e8c24c817c9f88451fb88c05160a33373ad3c2a
Khushbu Soni <khushbu@xavoc.com>
2014-07-06 15:53:36 +0530

    Working on day wise fee head report

---
+++ lib/View/FeesReport.php
@@ -0,0 +1,64 @@
+<?php
+
+class View_FeesReport extends View {
+	public $from_date=null;
+	public $to_date=null;
+	function init(){
+		parent::init();
+
+		if(!$this->from_date)
+			$this->from_date = date('Y-m-01',strtotime($this->api->today));
+		if(!$this->to_date)
+			$this->to_date = date('Y-m-t',strtotime($this->api->today));
+
+		$fees_transaction  = $this->add('Model_FeesTransaction');
+		$fees_transaction->addCondition('submitted_on','>=',$this->from_date);
+		$fees_transaction->addCondition('submitted_on','<',$this->api->nextDate($this->to_date));
+		$student_applied_fees_join = $fees_transaction->join('student_fees_applied','student_applied_fees_id');
+		$student_applied_fees_join->addField('fees_id');
+		// $fees_join = $student_applied_fees_join->join('fees','fees_id');
+
+		$fees_transaction->_dsql()->del('fields')
+			->field('submitted_on')
+			->field($student_applied_fees_join->table_alias.'.fees_id')
+			->field('by_consession')
+			->field('SUM(fees_transactions.amount) as total_amount');
+
+		 $fees_transaction->addCondition('by_consession',null);
+
+		$fees_transaction->_dsql()->group('submitted_on, _s.fees_id, fees_transactions.by_consession');
+		
+		// echo "<pre>";
+		// print_r($fees_transaction->_dsql()->get());
+		// echo "</pre>";
+
+		$result_array = array();
+		$grid=$this->add('Grid');
+		$columns_added=array();
+		$grid->addColumn('text','date');
+		$fees = $this->add('Model_Fees');
+		
+		foreach ($fees_transaction->_dsql()->get() as $data) {
+			$fees->unload();
+			$fees->load($data['fees_id']);
+                        
+            if(!isset($result_array[$data['submitted_on']])) $result_array[$data['submitted_on']] = array();
+                        
+			$result_array[$data['submitted_on']] += array(
+					'date'=>$data['submitted_on'],
+					$fees['name'] => $data['total_amount']
+				);
+			if(!in_array($fees['name'], $columns_added)){
+				$grid->addColumn('text',$fees['name']);
+				$columns_added[] = $fees['name'];
+			}
+		}
+		
+		$grid->setSource($result_array);
+		
+		// echo "<pre>";
+		// print_r($result_array);
+		// echo "</pre>";
+
+	}
+}
--- page/reports/fees.php
+++ page/reports/fees.php
@@ -9,6 +9,13 @@ class page_reports_fees extends Page {
 		$form->addField('DatePicker','to_date');
 		$form->addSubmit('Get List');
 
-		
+		$v=$this->add('View_FeesReport',array('from_date'=>$_GET['from_date'],'to_date'=>$_GET['to_date']));
+
+		if($form->isSubmitted()){
+			$v->js()->reload(array(
+					'from_date'=>$form['from_date']?:0,
+					'to_date'=>$form['to_date']?:0,
+				))->execute();
+		}
 	}
 }
