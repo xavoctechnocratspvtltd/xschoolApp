9d96518440feb7f2b172384c1fe353222a8ebfa0
Khushbu Soni <khushbu@xavoc.com>
2014-07-19 11:54:20 +0530

    before pull on client server

--- page/library/main.php
+++ page/library/main.php
@@ -9,7 +9,7 @@ class page_library_main extends Page{
 		$tab1=$tabs->addTabURL('library_subjects','Subjects');
 		$tab1=$tabs->addTabURL('library_title','Title');
 		$tab1=$tabs->addTabURL('library_item','Item');
-		$tab1=$tabs->addTabURL('library_stocktransaction','Stock transaction');
+		// $tab1=$tabs->addTabURL('library_stocktransaction','Stock transaction');
 		$tab1=$tabs->addTabURL('library_transaction','Library Actions');
 	}
 }
