fe500307e1ff4a3fbff1b66acc89c5e33d115fd3
Gowrav Vishwakarma <gowrav@gowrav-Lenovo-G570.(none)>
2014-08-27 18:47:27 +0530

    set original

--- config-default.php
+++ config-default.php
@@ -1,7 +1,7 @@
 <?php
 
 $config['atk']['base_path']='./atk4/';
-$config['dsn']='mysql://root:winserver@localhost/demo';
+$config['dsn']='mysql://root:winserver@localhost/xschool';
 
 $config['url_postfix']='';
 $config['url_prefix']='?page=';
