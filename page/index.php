8333ef4a081043ce97feb913c3f0c7fb11fce46e
Xavoc <xavoc@xavoc-To-be-filled-by-O-E-M.(none)>
2014-09-12 10:32:55 +0530

    before pull

--- page/index.php
+++ page/index.php
@@ -10,7 +10,7 @@ class page_index extends Page {
 
 
 
-        if(strtotime('2014-09-12')==strtotime(date('Y-m-d')))
+        if(strtotime('2014-09-16')==strtotime(date('Y-m-d')))
 
         throw new Exception('Software Licesne Expired, Kindly Contact To Your Service Provider');
 
