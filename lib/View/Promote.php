ac7eb64b0fc47ebb48489331545467a583c8256c
xavockhushbu <gowrav@gowrav-Lenovo-G570.(none)>
2014-09-18 10:44:07 +0530

    before push

--- lib/View/Promote.php
+++ lib/View/Promote.php
@@ -4,22 +4,24 @@ class View_Promote extends View{
 
 	function init(){
 		parent::init();
-		
-		$this->api->stickyGET('from_class');
+		$this->api->stickyGET('class');
+		$this->api->stickyGET('session');
+
+
 		$class=$this->add('Model_Class');
 		$class->load($_GET['class']);
-		$students=$class->students()->setOrder('id');
+		// $students=$class->students()->setOrder('id');
+		$students=$class->ref('Student')->addCondition('session_id',$_GET['session'])->setOrder('id');
 
 		$form=$this->add('Form');
 		$student_type=$this->add('Model_StudentType');
 		$i=1;
 		foreach ($students as $junk) {
-			$student_field=$form->addField('line','student_'.$i);
-			$student->load($students['id']);
-			$student_id_field=$form->addField('line','student_id'.$i);
+			$student_field=$form->addField('Readonly','student_'.$i);
+			$students->load($students['id']);
 			// $student_field->template->addSeprator('class','span6');
-			$student_field->set($student['name']);		
-			$student_id_field->set($student['id']);		
+			$student_field->set($students['name']);		
+
 			$student_type_field=$form->addField('dropdown','student_type_'.$i);
 			$student_type_field->setModel($student_type);		
 			$student_type_field->set($students['studenttype_id']);
@@ -27,18 +29,23 @@ class View_Promote extends View{
 		}		
 
 		$to_class_field=$form->addField('autocomplete/Basic','to_class');
-		$to_class_field->setModel('Class');
+		$to_class_model=$this->add('Model_Class');
+		$to_class_model->title_field='full_name';
+		$to_class_field->setModel($to_class_model);
 
 		$form->addSubmit('Promote');
 
 		if($form->isSubmitted()){
+			$to_class=$this->add('Model_Class');
+			$to_class->load($form['to_class']);
 
-				$to_class=$this->add('Model_Class')->tryLoad($form['to_class']);
+			$students=$class->ref('Student')->addCondition('session_id',$_GET['session'])->setOrder('id');
 				
-				$students=$class->students()->setOrder('id');
+				
 				$i=1;
 				foreach ($students as $junk) {
 					// $s=$students->allReadyInSession($class);
+					
 					$st=$this->add('Model_Student');
 					$scholar=$this->add('Model_Scholar');
 					$scholar->load($students['scholar_id']);
@@ -50,8 +57,10 @@ class View_Promote extends View{
 					$to_class->addStudent($scholar, $form['student_type_'.$i]);						
 						
 					$i++;
+
 				}
 				
+				$form->js()->univ()->successMessage('Promote successfully')->execute();	
 			}
 	}
 }
