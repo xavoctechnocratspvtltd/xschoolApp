7d3be44a2d99594c4caf2586f751e450b8467386
vijay mali <guddavijay@gmail.com>
2016-09-06 12:21:34 +0530

    check model loaded or not

--- page/staffAcl.php
+++ page/staffAcl.php
@@ -5,9 +5,14 @@ class page_staffAcl extends Page{
 		parent::init();
 		$staff_id=$this->api->stickyGET('staff_id');
 		$m=$this->add('Model_ACL');
-		$m->tryLoadBy('staff_id',$staff_id);
-		$m->getElement('page')->editable(false);
-		$c=$this->add('CRUD',array('allow_add'=>false,'allow_del'=>false));
-		$c->setModel($m);
+		$m->addCondition('staff_id',$staff_id);
+		$m->tryLoadAny();
+		if($m->loaded()){
+			$m->getElement('page')->editable(false);
+			$c=$this->add('CRUD',array('allow_add'=>false,'allow_del'=>false));
+			$c->setModel($m);
+		}else{
+			// $this->add('View_Info')->set('This Staff Not visit on this page');
+		}
 	}
 }
